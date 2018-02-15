1471fe40db5148ace38721b5eca7941ba2e689e6
unknown <karthikeyanpram@gmail.com>
2018-02-15 10:30:14 -0500

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var http = require('http');
+
+//create a server object:
+const server = http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+});
+server.listen(8080); //the server object listens on port 8080
+
+console.log("listening in loca; host");
