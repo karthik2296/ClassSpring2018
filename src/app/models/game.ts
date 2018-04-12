42f111bb63ac2ca60e73a597f0007dbcb865dd1d
unknown <karthikeyanpram@gmail.com>
2018-04-12 10:00:47 -0400

    flip picture

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -2,15 +2,11 @@
 
 
 export class Game {
-    MyQuotes: string[] = [
-        "Great minds think alike, but ...",
-        "Axis of Evil",
-        "There are two extremes"
-    ];
+
     Players: User[] = [
-        { Name: 'Moshe Plotkin' },
-        { Name: 'Donald Trump' },
-        { Name: 'Barak Obama' },        
+        { Name: 'Moshe Plotkin', MyQuotes: [] },
+        { Name: 'Donald Trump', MyQuotes: [] },
+        { Name: 'Barak Obama', MyQuotes: [] },        
     ];
     Dealer: string = "Moshe Plotkin";
     Picture: string = 'https://media3.s-nbcnews.com/j/msnbc/components/video/201803/tdy_news_welker_trump_180315_1920x1080.nbcnews-ux-1080-600.jpg';
@@ -21,6 +17,7 @@ export class Game {
 
 export class User {
     Name: string;
+    MyQuotes: string[];
 }
 
 export class Quote {
